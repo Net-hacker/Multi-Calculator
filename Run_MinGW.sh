30dba5e44b9fc1b9d2f563d06fda4e85fac2dc78
Juri Zockt <85522443+Juri-Zockt@users.noreply.github.com>
2022-10-23 16:49:25 +0200

    Update Run_MinGW.sh

--- Run_MinGW.sh
+++ Run_MinGW.sh
@@ -1,5 +1,5 @@
 clear
-gcc -I Logic -L -static All-Calc.cpp -o Mega-Calculator
+gcc -I Logic -L -static Multi-Calc.cpp -o Mega-Calculator
 read -p "Dücke [Enter] um fortzufahren..."
 clear
 echo "Script Made by Juri-Zockt"
